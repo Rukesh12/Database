9131560f3837a0b66ea5f3fa744f74316c95975e
WilcyWilson <wilsonshresthaapps@gmail.com>
2018-12-15 21:32:22 -0800

    Login Page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+<?php
+if(isset($_POST['login']))
+{
+$name=$_POST['name'];
+$password=md5($_POST['password']);
+$str="SELECT * FROM users WHERE username='$name' AND password='$password'";
+//making connection
+include('connection.php');
+//making query
+$qry=mysqli_query($con,$str);
+//counting the records
+$rec=mysqli_num_rows($qry);
+//counts the record in the database i.e only one username with same name //should be present(only one username so record=1)
+if($rec==1)
+{
+	header('Location: https://github.com');
+}
+else
+{
+	echo "Unable to Login";
+}
+}
+?>
+<form method="post" action="" name="userLogin" enctype="multipart/form_data">
+	<fieldset>
+<legend>Login</legend>
+<input type="text" name="name" placeholder="Username"><br/>
+<input type="password" name="password" placeholder="Password"><br/>
+<input type="submit" name="login" value="Login"><br/>
+</fieldset>
+</form>
+</body>
+</html>
